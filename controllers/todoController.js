d7b169e0a088b4ce288de5c6bcb5e82210fa9c23
Ryan Greenberg <hawk1138@gmail.com>
2017-04-27 14:22:57 -0700

    added proper delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -15,10 +15,6 @@ module.exports = function(app){
 	});
 
 	app.post('/todo', urlencodedParser, function(req, res){
-		console.log('an http post request was recieved at the endpoint/route "/todo"')
-		console.log("this is the req body", req.body)
-		console.log("this is typeof req body", typeof req.body)
-		console.log("this is the req body item, this is what we want to put in the database-->", req.body.item)
 
 		Tasks.create(req.body.item)
 		.then(todos => {
@@ -32,17 +28,21 @@ module.exports = function(app){
 	});
 
 	app.post('/todo/:item', function(req, res){
-		console.log('an http post request was recieved at the endpoint/route "/todo:item"')
 		Tasks.completed(req.params.item);
 		res.redirect('/todo');
 	});
 
-	app.delete('/todo/:item', function(req, res){
-		console.log('an http delete request was recieved at the endpoint/route "/todo:item"')
-		data = data.filter(function(todo){
-			return todo.item.replace(/ /g, '-') !== req.params.item;
-		});
-		res.json(data);
+	app.post('/delete/:item', function(req, res){
+		Tasks.delete(req.params.item);
+		res.redirect('/todo');
 	});
+
+	// app.delete('/todo/:item', function(req, res){
+	// 	console.log('an http delete request was recieved at the endpoint/route "/todo:item"')
+	// 	data = data.filter(function(todo){
+	// 		return todo.item.replace(/ /g, '-') !== req.params.item;
+	// 	});
+	// 	res.redirect('/todo');
+	// });
 
 };
