c0294a327fa487007320a415618652a7c6583eb6
Ryan Greenberg <hawk1138@gmail.com>
2017-04-28 13:56:26 -0700

    100% delinted

--- test/db.js
+++ test/db.js
@@ -1,28 +1,25 @@
-// const { expect } = require('chai');
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
 const db = require('../database/db.js');
 
-describe('db.js', function () {
-  describe('create task', function () {
-    it('puts a thing in the database', function (done) {
+describe('db.js', () => {
+  describe('create task', () => {
+    it('puts a thing in the database', (done) => {
       db.create('test item 1')
         .then(() => done())
         .catch(done);
     });
   });
-  describe('complete task', function () {
-    it('changes database item to complete', function (done) {
+  describe('complete task', () => {
+    it('changes database item to complete', (done) => {
       db.getAll()
-        .then((todone) => {
-          return db.completed(todone[0].id);
-        })
+        .then(todone => db.completed(todone[0].id))
         .then(() => done())
         .catch(done);
     });
   });
-  describe('edit task', function (){
-    it('changes the text of the task', function (done){
+  describe('edit task', () => {
+    it('changes the text of the task', (done) => {
       db.getAll()
       .then((toedit) => {
         db.edited(toedit[0].id, 'edited task')
@@ -30,20 +27,18 @@ describe('db.js', function () {
           editedPromise
           .then((result) => {
             expect(result).to.equal('edited task');
-          })
-        })
+          });
+        });
       })
       .then(() => done())
       .catch(done);
     });
   });
 
-  describe('delete task', function () {
-    it('delete database item', function (done) {
+  describe('delete task', () => {
+    it('delete database item', (done) => {
       db.getAll()
-      .then((todone) => {
-        return db.delete(todone[0].id);
-      })
+      .then(todone => db.delete(todone[0].id))
       .then(() => done())
       .catch(done);
     });
